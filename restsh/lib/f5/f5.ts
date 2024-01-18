cbf15bf24a9fa65131a1383784539bdc9b377daa
Juergen <juergen.mang@axians.de>
2024-01-18 15:49:00 +0100

    Upd: support f5 telemetrx streaming

---
+++ restsh/lib/f5/f5.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Author: Juergen Mang <juergen.mang@axians.de>
+# Date: 2024-01-17
+
+# Shortdesc: General functions for the F5 Telemetry Streaming module.
+# Desc:
+# General functions for the Telemetry Streaming AS3 module.
+
+# Strict error handling
+set -uo pipefail
+
+# Debug mode
+[ -n "${RESTSH_DEBUG+x}" ] && set -x
+
+# Returns the installed TS version
+f5.ts.version() {
+    GET "/mgmt/shared/telemetry/info"
+}
+
+export -f f5.ts.version
